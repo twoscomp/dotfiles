4269b1ecccee5ca1e127b610290e545bb0bfd448
Daniel Lin <linonmail@gmail.com>
2024-04-23 10:49:18 -0500

    update to new fish shell setup

---
+++ .config/fish/completions/asdf.fish
@@ -0,0 +1,1 @@
+/home/dlin/.asdf/completions/asdf.fish
---
+++ .config/fish/config.fish
@@ -0,0 +1,5 @@
+if status is-interactive
+    # Commands to run in interactive sessions can go here
+    direnv hook fish | source
+    source ~/.asdf/asdf.fish
+end
