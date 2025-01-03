55341b5c457c52c482c473010e0400f5991102df
Daniel Lin <linonmail@gmail.com>
2025-01-02 23:03:39 -0600

    Installed fzf on laptop and update lazynvim.

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -2,4 +2,6 @@ if status is-interactive
     # Commands to run in interactive sessions can go here
     direnv hook fish | source
     source ~/.asdf/asdf.fish
+    # Set up fzf key bindings
+    fzf --fish | source
 end
