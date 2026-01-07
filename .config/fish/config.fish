a295e59d981cce2933154e42db5145a88d874467
Daniel Lin <linonmail@gmail.com>
2026-01-06 22:11:42 -0600

    backing up wsl config for bazzite trial

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,7 +1,17 @@
 if status is-interactive
     # Commands to run in interactive sessions can go here
     direnv hook fish | source
-    source ~/.asdf/asdf.fish
+    # ASDF v0.16.0 configuration
+    fish_add_path "$HOME/.local/bin"
+    if test -z $ASDF_DATA_DIR
+        set _asdf_shims "$HOME/.asdf/shims"
+    else
+        set _asdf_shims "$ASDF_DATA_DIR/shims"
+    end
+    fish_add_path $_asdf_shims
     # Set up fzf key bindings
     fzf --fish | source
+
+    # Gemini API Key for Avante.nvim (Scoped)
+    set -gx AVANTE_GEMINI_API_KEY "AIzaSyDeMSfFnrKURnccuQfu5lkrQ0YMW6kHUjs"
 end
